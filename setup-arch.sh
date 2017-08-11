b361c890452df625a5b0ea2459f49560f60805b6
Christoph Heiss <me@christoph-heiss.me>
2017-08-11 23:44:02 +0200

    Arch: Update packages to install.

--- setup-arch.sh
+++ setup-arch.sh
@@ -19,14 +19,12 @@ gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
 pacaur -Syu --aur --noconfirm
 pacaur --noconfirm --noedit -S \
           scrot thefuck dropbox youtube-dl markdown speedometer htop \
-          nodejs npm texlive-most gimp screenfetch geary nmap avahi \
+          nodejs yarn texlive-most gimp screenfetch geary nmap avahi \
           sublime-text-dev intel-ucode cups cups-filters ghostscript \
           ttf-meslo fontconfig vlc libva-vdpau-driver spotify cmake \
           iotop perl-mime-tools perl-net-smtp-ssl perl-authen-sasl \
-          qemu bash-completion cower
+          qemu qemu-arch-extra bash-completion cower google-chrome 
 
 pacaur -Qs hplib && pacaur -Rns hplib
-
-sudo npm install -g gulp
 
 cp -va files/.bash_{aliases,colors,profile,utils} files/.bashrc files/.gitconfig $HOME/
