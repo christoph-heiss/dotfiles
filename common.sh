cd6be1c840dca159ae0f89e42357e2bd3eb3946a
Christoph Heiss <me@christoph-heiss.me>
2021-05-15 00:23:45 +0200

    setup: clean up `common.sh` a bit
    Signed-off-by: Christoph Heiss <me@christoph-heiss.me>

--- common.sh
+++ common.sh
@@ -3,15 +3,16 @@
 set -e
 set -u
 
-[[ ! -d $HOME/.config/base16-shell ]] && git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell || true
 [[ ! -d $HOME/.oh-my-zsh ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp -vp files/.{alacritty.yml,gitconfig,tmux.conf,zshrc} $HOME/
 
 touch $HOME/.z
 
+[[ ! -d $HOME/.config/base16-shell ]] && git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell || true
+
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 [[ ! -d $ZSH_CUSTOM/themes/spaceship-prompt ]] && git clone https://github.com/denysdovhan/spaceship-prompt.git $ZSH_CUSTOM/themes/spaceship-prompt || true
 ln -sf $ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme $ZSH_CUSTOM/themes/spaceship.zsh-theme
 
-[[ ! -d $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ]] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting  || true
+[[ ! -d $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ]] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting || true
